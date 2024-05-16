9949c4ed1f0612811a673180f01b0952d37207c4
huebitoo <camposjavier143@gmail.com>
2024-05-15 21:59:29 -0400

    Implementacion de clases Ausente, clase abstracta Reunion y el Informe.txt junto a los JUnit Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,61 @@
 package org.example;
 
+import java.io.*;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Departamentos de ejemplo
+        Departamento dep1 = new Departamento("dep1");
+        Departamento dep2 = new Departamento("dep2");
+
+        // Empleados de ejemplo
+        Empleado emp1 = new Empleado("1", "Oliver1", "Penailillo1", "hola1@123.cl");
+        Empleado emp2 = new Empleado("2", "Oliver2", "Penailillo2", "hola2@123.cl");
+        Empleado emp3 = new Empleado("3", "Oliver3", "Penailillo3", "hola3@123.cl");
+        Empleado emp4 = new Empleado("4", "Oliver4", "Penailillo4", "hola4@123.cl");
+
+        // Asignación departamentos
+        dep1.addEmpleados(emp1);
+        dep1.addEmpleados(emp2);
+        dep2.addEmpleados(emp3);
+        dep2.addEmpleados(emp4);
+
+        // Creación de la reunión
+        ReunionPresencial reunion = new ReunionPresencial(LocalDate.of(2024, 5, 7), LocalDateTime.of(2024, 5, 7, 10, 0), Duration.ofMinutes(120), "A411", emp1, TipoReunion.MARKETING.getString());
+
+        // Invitaciones a usuarios y departamentos
+        emp1.invitar(reunion, LocalDateTime.of(2024, 5, 7,10,0));
+        emp2.invitar(reunion, LocalDateTime.of(2024, 5, 7,10,0));
+        dep2.invitar(reunion, LocalDateTime.of(2024, 5, 7,10,0));
+
+        // Iniciar reunión
+        reunion.iniciar(LocalDateTime.of(2024, 5, 7, 10, 0));
+
+        // Confirmacion Asistencia
+        emp1.confirmarAsistencia(LocalDateTime.of(2024,5,7,10,0), reunion);
+        emp2.confirmarAsistencia(LocalDateTime.of(2024,5,7,11,0), reunion);
+        emp3.confirmarAsistencia(LocalDateTime.of(2024,5,7,9,40), reunion);
+
+        //Notas
+        reunion.notas.add(new Nota("Primera nota"));
+        reunion.notas.add(new Nota("Segunda nota"));
+        reunion.notas.add(new Nota("Tercera nota"));
+
+        // test
+        ArrayList<Invitacion> invitaciones = reunion.listaInvitaciones();
+        ArrayList<Presente> presentes = reunion.obtenerAsistencias();
+        ArrayList<Retraso> retrasos = reunion.obtenerRetrasos();
+        ArrayList<Ausente> ausentes = reunion.obtenerAusentes();
+
+        // Finalizacion de la reunión
+        reunion.finalizar(LocalDateTime.of(2024, 5, 7, 11, 30));
+
+        // Lista Ausentes
+        reunion.obtenerAusentes();
     }
 }
---
+++ src/main/java/org/example/Reunion.java
@@ -0,0 +1,235 @@
+package org.example;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+/**
+ * Clase Reunión
+ */
+
+
+abstract public class Reunion {
+    /**
+     * Variable para almacenar la fecha
+     */
+    private LocalDate fecha;
+    /**
+     * Variable para almacenar la hora de inicio prevista
+     */
+    public LocalDateTime horaPrevista;
+    /**
+     * Variable para almacenar la duracion prevista
+     */
+    public Duration duracionPrevista;
+    /**
+     * Variable para almacenar la ubicación
+     */
+    private String ubicacion;
+    /**
+     * Variable para almacenar la hora de inicio
+     */
+    public LocalDateTime horaInicio;
+    /**
+     * Variable para almacenar la hora de finalización
+     */
+    public LocalDateTime horaFin;
+    /**
+     * Clase Empleado para almacenar al organizador
+     */
+    public Empleado organizador;
+    /**
+     * ArrayList de Notas para almacenar las notas durante la reunión
+     */
+    public ArrayList<Nota> notas;
+    /**
+     * ArrayList de Invitaciones para almacenar los invitados
+     */
+    public ArrayList<Invitacion> invitaciones;
+    /**
+     * ArrayList de invitados presentes
+     */
+    public ArrayList<Presente> presentes;
+    /**
+     * ArrayList de invitados quellegaron Atrasados
+     */
+    public ArrayList<Retraso> retrasos;
+    /**
+     * ArrayList de invitados ausentes
+     */
+    public ArrayList<Ausente> ausentes;
+    /**
+     * Variable que almacenará el tipo de reunión
+     */
+    private String tipo_reunion;
+
+    /**
+     * Constructor Reunion
+     *
+     * @param fecha            fecha de creacion de la reunion
+     * @param horaPrevista     inicio de la reunion
+     * @param duracionPrevista duracion de la reunion
+     * @param ubicacion        la sala o enlace
+     */
+    Reunion(LocalDate fecha, LocalDateTime horaPrevista, Duration duracionPrevista, String ubicacion, Empleado organizador, String tipodereunion) {
+        this.fecha = fecha;
+        this.horaPrevista = horaPrevista;
+        this.duracionPrevista = duracionPrevista;
+        this.ubicacion = ubicacion;
+        this.organizador = organizador;
+        this.tipo_reunion = tipodereunion;
+
+        notas = new ArrayList<>();
+        invitaciones = new ArrayList<>();
+        presentes = new ArrayList<>();
+        retrasos = new ArrayList<>();
+        ausentes = new ArrayList<>();
+
+    }
+
+    /**
+     * Método que da inicio a la reunion
+     * @param horaInicioReal Hora de inicio de la reunion
+     */
+    public void iniciar(LocalDateTime horaInicioReal) { horaInicio = horaInicioReal; }
+
+    /**
+     * Método que da fin a la reunion
+     * @param horaFinReal Hora de fin de la reunion
+     */
+    public void finalizar(LocalDateTime horaFinReal) {
+        horaFin = horaFinReal;
+
+        for(Invitacion i : invitaciones){
+            int p_si = 1;
+            int r_si = 1;
+            int a_si = 1;
+
+            for(Presente p : presentes){
+                if(i.empleado.equals(p.usuario)){
+                    p_si = 0;
+                }
+            }
+
+            for(Retraso r : retrasos){
+                if(i.empleado.equals(r.usuario) ){
+                    r_si = 0;
+                }
+            }
+
+            for(Ausente a : ausentes){
+                if(i.empleado.equals(a.usuario) ){
+                    a_si = 0;
+                }
+            }
+            if(r_si == 1 && p_si == 1 && a_si == 1){
+                ausentes.add(new Ausente(i.empleado));
+            }
+        }
+
+        crearInforme();
+
+    }
+
+    /**
+     * Método para calcular la duracion de la reunion
+     * @return Retorna un float que representa la duracion de la reunion en minutos
+     */
+    public float calcularTiempoReal() {
+        // Se devuelve el tiempo que duró la reunión
+        Duration duracion = Duration.between(horaInicio, horaFin);
+        return (float) duracion.toMinutes();
+    }
+
+    /**
+     * Metodo para mostrar los invitados
+     * @return Retorna un ArrayList con los invitados
+     */
+    public ArrayList<Invitacion> listaInvitaciones(){
+        return invitaciones;
+    }
+
+    /**
+     * Metodo para obtener las Asistencias
+     * @return Retorna un ArrayList por los presentes
+     */
+    public ArrayList<Presente> obtenerAsistencias(){
+        return presentes;
+    }
+    /**
+     * Método para obtener la lista de atrasos
+     * @return Retorna un ArrayList con los atrasos
+     */
+    public ArrayList<Retraso> obtenerRetrasos(){
+        return retrasos;
+    }
+    /**
+     * Metodo para obtener la lista de los ausentes
+     * @return Retorna un ArrayList con los invitados ausentes
+     */
+    public ArrayList<Ausente> obtenerAusentes(){
+        return ausentes;
+    }
+    /**
+     * Metodo que crea el Informe que será almacenado en un archivo TXT al final de la reunión
+     */
+    public void crearInforme(){
+        String nombrearchivo = "src/main/java/org/example/Informe.txt";
+
+        File archivo = new File(nombrearchivo);
+
+        try {
+            PrintWriter escritor = new PrintWriter(archivo);
+            escritor.println("Informe Reunión " + fecha + "\nOrganizado por: " + organizador);
+            escritor.println("Hora de la reunion: " + horaPrevista);
+            escritor.println("Hora finalización: " + horaFin);
+            escritor.println("Duracion total: " + calcularTiempoReal() + " minutos");
+            escritor.println("Tipo de reunión: " + tipo_reunion);
+            escritor.println("Ubicación: " + ubicacion);
+
+            escritor.println("\n");
+            escritor.println("-------- Lista de presentes ---------\n");
+            int i = 1;
+            for (Presente e : presentes) {
+                escritor.println(i + "._Rli" + e.usuario.toString());
+                i++;
+            }
+            i = 1 ;
+            escritor.println("\n");
+
+            escritor.println("-------- Lista de Atrasados ---------\n" );
+
+            for(Retraso r : retrasos){
+                escritor.println(i + "._ " + r.usuario.toString());
+                i++;
+            }
+
+            i = 1;
+            escritor.println("\n");
+
+            escritor.println("-------- Lista de Ausentes ---------\n" );
+            for(Ausente a : ausentes){
+                escritor.println(i + "._ " + a.usuario.toString());
+                i++;
+            }
+
+            i = 1;
+            escritor.println("\n");
+
+            escritor.println("-------- Notas ---------\n");
+            for(Nota n : notas){
+                escritor.println(i + "._ " + n.getNota());
+                i++;
+            }
+
+            escritor.close();
+        } catch (Exception e){
+            System.err.println("Error: " + e);
+        }
+
+    }
+}
