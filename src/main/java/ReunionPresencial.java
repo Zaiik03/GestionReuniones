bc6175033338c32586db9f54663f0ded080f1e7c
Matygp <matgarcia2023@inf.udec.cl>
2024-05-15 20:28:35 -0400

    Implementación clase ReunionPresencial

---
+++ src/main/java/ReunionPresencial.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * Subclase Reunión Presencial
+ */
+public class ReunionPresencial extends Reunion{
+    /**
+     * Constructor ReunionPresecial
+     * @param fecha Fecha de la reunión
+     * @param horaPrevista Hora prevista de la reuníón
+     * @param duracionPrevista Duración prevista de la reunión
+     * @param ubicacion Ubicación de la reunión (Sala, local, url, enlace)
+     * @param organizador Organizador de la Reunión
+     * @param tipodereunion Especifica el tipo de reunión (Marketing, tecnica, otros...)
+     */
+    public ReunionPresencial(LocalDate fecha, LocalDateTime horaPrevista, Duration duracionPrevista, String ubicacion, org.example.Empleado organizador, String tipodereunion){
+        super(fecha, horaPrevista, duracionPrevista, ubicacion, organizador, tipodereunion);
+    }
+}
